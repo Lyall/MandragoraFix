630798af4e37f5295673fa377d6787630d745618
Lyall <lyall@stamper.net>
2025-04-20 02:22:23 +0100

    Enable build optimisations.

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,21 @@
 set_project("MandragoraFix")
 add_rules("mode.debug", "mode.release")
 set_languages("cxxlatest", "clatest")
-set_optimize("faster")
+set_optimize("smallest")
 
   target("MandragoraFix")
     set_kind("shared")
-    add_files("src/*.cpp", "src/SDK/UMG_functions.cpp", "src/SDK/CoreUObject_functions.cpp", "src/SDK/Basic.cpp", "external/safetyhook/safetyhook.cpp", "external/safetyhook/Zydis.c")
+    add_files("src/*.cpp", "src/SDK/Engine_functions.cpp", "src/SDK/UMG_functions.cpp", "src/SDK/CoreUObject_functions.cpp", "src/SDK/Basic.cpp", "external/safetyhook/safetyhook.cpp", "external/safetyhook/Zydis.c")
     add_syslinks("user32")
     add_includedirs("external/spdlog/include", "external/inipp", "external/safetyhook")
     set_prefixname("")
     set_extension(".asi")
-
+    
   -- Set platform specific toolchain
   if is_plat("windows") then
     set_toolchains("msvc")
-    add_cxflags("/utf-8")
+    add_cxflags("/utf-8", "/GL")
+    add_ldflags("/LTCG", "/OPT:REF", "/OPT:ICF")
     if is_mode("release") then
       add_cxflags("/MT")
     elseif is_mode("debug") then
